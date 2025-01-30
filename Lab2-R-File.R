abb960ac108bfeaa110e03cdae5203f9c347eea3
Cosmic-dis <cagracoss@gmail.com>
2025-01-30 15:27:28 -0500

    finished task 1 step 4

--- Lab2-R-File.R
+++ Lab2-R-File.R
@@ -36,7 +36,7 @@ for (sub in sub.direc){
   music = list.files(sub)
   for (wav in music){
     file = paste(sub, wav, sep = "/")
-    print(file)
+    # print(file)
     remove = str_sub(wav, 1, length(wav)-6)
     name = str_split(remove, "-")
     new.name = name[[1]][length(name[[1]])]
@@ -46,8 +46,21 @@ for (sub in sub.direc){
     only.album = album[[1]][length(album[[1]])]
     output = paste(artist, "-", only.album, "-", new.name,".json", sep = "")
     #print(output)
-    exe = paste("streaming_extractor_music.exe", '"', file, '"', '"', output, '"', " ")
-    print(exe)
+    exe = paste("streaming_extractor_music.exe", ' "', new.name,".wav", '" ', '"', output, '"', sep = "")
+    code.to.process = append(code.to.process, exe)
+    # print(code.to.process)
   }
 }
-  
+# print(code.to.process)
+
+# Step 4
+
+writeLines(code.to.process, "batfile.txt")
+
+# Task 2
+
+# Step 1
+
+
+
+# Task 
